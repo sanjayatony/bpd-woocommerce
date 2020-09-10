c66fe9f8046e61d0441585511d9d7c52ec18108f
Tony S <sanjayatony@gmail.com>
2020-09-10 16:31:18 +0700

    add init function to add the methods

--- bpd-woocommerce.php
+++ bpd-woocommerce.php
@@ -3,5 +3,22 @@
  * Plugin Name: BPD - WooCommerce Payment gateway
  * Version: 0.5
  * Author: BPD IT Team
- * this plugin written based on https://docs.woocommerce.com/document/payment-gateway-api/
+ * Written based on https://docs.woocommerce.com/document/payment-gateway-api/
  */
+
+/**
+ * Init.
+ */
+function bpd_gateway_init() {
+	include 'class/class.bpd-gateway-qris.php';
+}
+add_action( 'plugins_loaded', 'bpd_gateway_init' );
+
+/**
+ * Add method to WooCommerce
+ */
+function add_bpd_payment_gateway( $methods ) {
+	$methods[] = 'WC_Gateway_Bpd_Qris';
+	return $methods;
+}
+add_filter( 'woocommerce_payment_gateways', 'add_bpd_payment_gateway' );
