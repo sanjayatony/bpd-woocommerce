7ba214c2dd77fcd6279e9efe206365a9dd0f47ba
Tony S <sanjayatony@gmail.com>
2020-09-10 14:37:14 +0700

    plugin init

---
+++ bpd-woocommerce.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: BPD - WooCommerce Payment gateway
+ * Version: 0.5
+ * Author: BPD IT Team
+ * this plugin written based on https://docs.woocommerce.com/document/payment-gateway-api/
+ */
