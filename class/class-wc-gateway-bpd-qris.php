11285dfe8c59b74586b398fd7f00a49fbce21625
Tony S <sanjayatony@gmail.com>
2020-09-15 00:30:50 +0700

    generate + process

--- class/class-wc-gateway-bpd-qris.php
+++ class/class-wc-gateway-bpd-qris.php
@@ -35,9 +35,10 @@ class WC_Gateway_BPD_QRIS extends WC_Payment_gateway {
 		$this->environment   = $this->get_option( 'environment' );
 		$this->merchant_code = $this->get_option( 'merchant_code' );
 		if ( 'sandbox' === $this->environment ) {
-			$this->merchant_id  = $this->get_option( 'merchant_id_sandbox' );
-			$this->merchant_key = $this->get_option( 'merchant_key_sandbox' );
-			$this->api_endpoint = 'http://36.75.213.124:7070/merchant-admin/rest/openapi/generateQrisPost';
+			$this->merchant_id       = $this->get_option( 'merchant_id_sandbox' );
+			$this->merchant_key      = $this->get_option( 'merchant_key_sandbox' );
+			$this->api_qris_endpoint = 'http://36.75.213.124:7070/merchant-admin/rest/openapi/generateQrisPost';
+			$this->api_soap_endpoint = 'http://36.75.213.124:7070/ws_bpd_payment/interkoneksi/v1/ws_interkoneksi.php?wsdl';
 		} else {
 			$this->merchant_id  = $this->get_option( 'merchant_id_production' );
 			$this->merchant_key = $this->get_option( 'merchant_key_production' );
@@ -83,6 +84,18 @@ class WC_Gateway_BPD_QRIS extends WC_Payment_gateway {
 				'description' => __( 'This controls the instruction which the user sees in thankyou page', 'woocommerce' ),
 				'default'     => __( 'Silahkan bayar dengan QRIS ini.' ),
 				'desc_tip'    => true,
+			),
+			'merchant_code'           => array(
+				'title'       => __( 'Merchant Code', 'woocommerce' ),
+				'type'        => 'text',
+				'description' => __( 'Enter your Merchant Code.', 'woocommerce' ),
+				'default'     => '',
+			),
+			'terminal'                => array(
+				'title'       => __( 'Terminal', 'woocommerce' ),
+				'type'        => 'text',
+				'description' => __( 'Enter your Terminal.', 'woocommerce' ),
+				'default'     => '',
 			),
 			'environment'             => array(
 				'title'       => __( 'Environment', 'woocommerce' ),
@@ -134,5 +147,68 @@ class WC_Gateway_BPD_QRIS extends WC_Payment_gateway {
 		wp_enqueue_script( 'bpd-admin-js', plugin_dir_url( __FILE__ ) . '../js/admin.js', array( 'jquery' ), '0.1', true );
 	}
 
+	/**
+	 * Process the payment and return the result
+	 *
+	 * @param int $order_id
+	 * @return array
+	 */
+	public function process_payment( $order_id ) {
+		global $woocommerce, $wpdb;
+		$order    = new WC_ORDER( $order_id );
+		$response = $this->generate_request( $order_id );
+		$logger   = wc_get_logger();
+		$logger->log( 'response generate', wc_print_r( $response, true ) );
 
+		if ( '00' === $response->code ) {
+			// Update order status.
+			$order->update_status( 'on-hold', 'Awaiting payment via ' . $this->method_title );
+			// Update order note with payment code.
+			$order->add_order_note( 'Your ' . $this->method_title . ' code is <b>' . $response->recordId . '</b>' );
+			// Save recordId in post meta.
+			add_post_meta( $order_id, '_recordId', $response->recordId, true );
+
+			return array(
+				'result'   => 'success',
+				'redirect' => $this->get_return_url( $order ),
+			);
+		} else {
+			wc_add_notice( $this->method_title . '  error:' . $response->status_code, 'error' );
+			return;
+
+		}
+
+	}
+
+	/**
+	 * Generate request to the API
+	 *
+	 * @param int $order_id
+	 */
+	public function generate_request( $order_id ) {
+		global $woocommerce;
+		$order = new WC_Order( $order_id );
+
+		$args   = array(
+			'username'  => $this->merchant_id,
+			'password'  => $this->merchant_key,
+			'noid'      => $order->get_order_number(),
+			'nama'      => $order->billing_first_name . ' ' . $order->billing_last_name,
+			'tagihan'   => round( $order->get_total(), 0 ),
+			'instansi'  => $this->merchant_code,
+			'ket_1_val' => $order->get_billing_address_1(),
+			'ket_2_val' => 'COCO ONLINE',
+			'ket_3_val' => $order->billing_phone,
+			'ket_4_val' => 'Transaksi COCO Online',
+			'ket_5_val' => round( $order->get_total(), 0 ),
+		);
+		$logger = wc_get_logger();
+		$logger->log( 'DATA INSERT', wc_print_r( $args, true ) );
+
+		// Connect to WSDL.
+		$client   = new SoapClient( $this->api_soap_endpoint );
+		$response = $client->__soapCall( 'ws_tagihan_insert', $args );
+		$logger->log( 'INSERT RESPONSE', wc_print_r( $response, true ) );
+		return json_decode( $response );
+	}
 }
